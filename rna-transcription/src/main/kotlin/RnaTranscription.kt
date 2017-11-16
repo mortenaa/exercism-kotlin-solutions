48b3eacbb36fd7eec18de772c46e1b58752452e5
Morten Nygaard Åsnes <Morten Nygaard snes>
2017-11-16 18:49:38 +0100

    rna-transcription

--- rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,12 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+    return dna.map {
+        when (it) {
+            'G' -> 'C'
+            'C' -> 'G'
+            'T' -> 'A'
+            'A' -> 'U'
+            else -> dna
+        }
+    }.joinToString(separator = "")
+}
+
